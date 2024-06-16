22fa429f82ba20e1bf644fb8e3151d81ab75ef22
vdi-terminal <vdi-terminal@EDU-WSB.local>
2024-06-16 15:09:44 +0200

    Initial commit

---
+++ Komendy.sh
@@ -0,0 +1,20 @@
+# Przejdź do katalogu projektu
+cd C:\Users\vdi-terminal\Desktop\projekt
+
+# Zainicjuj lokalne repozytorium Git
+git init
+
+# Dodaj wszystkie pliki do repozytorium
+git add .
+
+# Zatwierdź dodane pliki
+git commit -m "Initial commit"
+
+# Dodaj zdalne repozytorium GitHub
+git remote add origin https://github.com/ReXxX-69/Lista-3  
+
+# Utwórz i zmień nazwę gałęzi na main
+git branch -M main
+
+# Prześlij pliki do zdalnego repozytorium
+git push -u origin main
