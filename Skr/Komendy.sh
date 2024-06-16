4c6ecc9304e8c1c72f50bfa007afb0c4819e989c
vdi-terminal <vdi-terminal@EDU-WSB.local>
2024-06-16 15:46:21 +0200

    Pierwszy2

---
+++ Skr/Komendy.sh
@@ -0,0 +1,13 @@
+C:\Users\vdi-terminal\Desktop\projekt
+
+git init
+dir
+git add .
+
+git commit -m "Initial commit"
+
+git remote add origin https://github.com/ReXxX-69/Lista-3  
+
+git branch -M main
+
+git push -u origin main
